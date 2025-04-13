f087ea1dc1ae1740d4aa49a7cd0b192c01f34a9f
yashvendra90 <yashvendra.singh_cs24@gla.ac.in>
2025-04-13 22:05:49 +0530

    Add files via upload

---
+++ Yashvendra.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"calculator.h"
+int main(){
+	int q,w;
+	scanf("%d",&q);
+	scanf("%d",&w);
+	printf("%d",mul(q,w));
+	printf("\n");
+	printf("%d",div(q,w));
+	printf("\n");
+    printf("%d",squ(q,w));
+ 
+
+
+}
---
+++ calc.h
@@ -0,0 +1,10 @@
+int mul(int x,int y){
+	return x*y;
+}
+int div(int x, int y){
+	return x/y;
+}
+int squ(int x,int y){
+	return x*x;
+	return y*y;
+}
