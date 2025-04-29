7013f2279e96524e11eec6a6aae517a241e94983
yashvendra90 <yashvendra.singh_cs24@gla.ac.in>
2025-04-29 18:04:43 +0530

    Update calc.h

--- calc.h
+++ calc.h
@@ -1,10 +1,13 @@
-int mul(int x,int y){
-	return x*y;
+typedef int (*operation_func)(int, int);
+int mul(int x, int y) {
+    return x * y;
 }
-int div(int x, int y){
-	return x/y;
+int divide(int x, int y) {
+    return x / y;
 }
-int squ(int x,int y){
-	return x*x;
-	return y*y;
+int squ(int x, int y) {
+    return x * x;  
 }
+operation_func op_mul = mul;
+operation_func op_div = divide;
+operation_func op_squ = squ;
